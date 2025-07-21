f0c7b4aecacb12529939cddb4e050c0852ca24da
Yra <y.seryakov@bilaboratory.com>
2025-07-21 14:19:23 +0300

    удалил две папки

--- Business.Data/Enum/PlatformEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Business.Data.Enum
-{
-    public enum PlatformEnum
-    {
-        Ps4,
-        Ps5,
-        Ps4_Ps5
-    }
-}
--- Business.Data/Models/Platform.cs
+++
@@ -1,9 +0,0 @@
-﻿using Business.Data.BaseEntities;
-
-namespace Business.Data.Models
-{
-    public class Platform:BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
